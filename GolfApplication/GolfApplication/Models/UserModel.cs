9f727230ffd55765639d9fb700a180560d8b3ddc
Sunil A <sunilkumar.attada@gmail.com>
2019-11-20 14:43:54 +0530

    ListUser Model Changes

--- GolfApplication/GolfApplication/Models/UserModel.cs
+++ GolfApplication/GolfApplication/Models/UserModel.cs
@@ -61,9 +61,39 @@ namespace GolfApplication.Models
         public bool? isModerator { get; set; }
     }
 
-    public class userList : getUser
+    public class userList 
     {
+        public int userId { get; set; }
+        public int userWithTypeId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+        public string dob { get; set; }
+        public string profileImage { get; set; }
+        public string phoneNumber { get; set; }
+        public string password { get; set; }
+        //public int countryId { get; set; }
+        //public int stateId { get; set; }
+        public string city { get; set; }
+        public string address { get; set; }
+        public string pinCode { get; set; }
+        [DefaultValue(false)]
+        public bool? isEmailNotification { get; set; }
+        [DefaultValue(false)]
+        public bool? isSMSNotification { get; set; }
+        [DefaultValue(false)]
+        public bool? isPublicProfile { get; set; }
+        public string userTypeId { get; set; }
         public string userType { get; set; }
+        public string countryName { get; set; }
+        public string stateName { get; set; }
+        public string userCreatedDate { get; set; }
+        public string userUpdatedDate { get; set; }
+        [DefaultValue(false)]
+        public bool? isEmailVerified { get; set; }
+        [DefaultValue(false)]
+        public bool? isPhoneVerified { get; set; }
     }
 
     public class otpVerify
